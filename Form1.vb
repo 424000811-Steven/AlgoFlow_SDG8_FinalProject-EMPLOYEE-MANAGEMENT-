f4b02924a05895e0093c0f29c57ed5ad642e34d2
424000811-Steven <424000811@ntc.edu.ph>
2026-05-22 23:35:33 -0700

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form1
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtPassword.PasswordChar = "*"c
+    End Sub
+
+    Private Sub chkShowPassword_CheckedChanged(sender As Object, e As EventArgs) Handles chkShowPassword.CheckedChanged
+        txtPassword.PasswordChar = If(chkShowPassword.Checked, ControlChars.NullChar, "*"c)
+    End Sub
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        If String.IsNullOrWhiteSpace(txtEmployeeID.Text) OrElse String.IsNullOrWhiteSpace(txtPassword.Text) Then
+            MessageBox.Show("Please fill in all required fields.")
+            Exit Sub
+        End If
+
+        If txtPassword.Text = "admin" Then
+
+            Form2.currentLoggedInEmpID = txtEmployeeID.Text
+            Dim f2 As New Form2()
+            f2.Show()
+            Me.Hide()
+        Else
+            MessageBox.Show("Wrong Password!")
+        End If
+    End Sub
+
+
+
+End Class
---
+++ ReportForm.vb
@@ -0,0 +1,22 @@
+﻿Imports Microsoft.Reporting.WinForms
+
+Public Class ReportForm
+
+    Private Sub ReportForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim ds As New AttendanceDBDataSet
+        Dim ta As New AttendanceDBDataSetTableAdapters.AttendanceTableAdapter
+
+        ta.Fill(ds.Attendance)
+
+        ReportViewer1.LocalReport.DataSources.Clear()
+
+        Dim rds As New ReportDataSource("EmployeeAttendance", ds.Tables("Attendance"))
+
+        ReportViewer1.LocalReport.DataSources.Add(rds)
+
+        ReportViewer1.RefreshReport()
+
+    End Sub
+
+End Class
